2e9e3c02621e56b179fd45277a15a3f3cf07f3f6
Chris Nobles <chris.l.nobles@gmail.com>
2017-06-06 17:13:56 -0400

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,6 +4,7 @@ conda config --prepend channels 'bushmanlab'
 conda config --prepend channels 'r'
 conda config --prepend channels 'bioconda'
 conda create -n guide-seq-pipe --file ./setup/requirements.txt --yes
+sleep 1
 source activate guide-seq-pipe
 cd scripts/
 git clone https://github.com/cnobles/dualDemultiplexR.git
@@ -11,4 +12,4 @@ git clone https://github.com/cnobles/seqTrimR.git
 git clone https://github.com/cnobles/seqConsolidateR.git
 git clone https://github.com/cnobles/blatCoupleR.git
 cd ../
-Rscript ./setup/bioconductor_setup.R
+#Rscript ./setup/bioconductor_setup.R
