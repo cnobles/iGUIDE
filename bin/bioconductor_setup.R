43c649452c5bba6fa0e5b37b76edd8dcf3b012b8
Christopher Nobles <cnobles@upenn.edu>
2017-12-06 11:03:24 -0500

    Changes to file structure. Added licence.

---
+++ bin/bioconductor_setup.R
@@ -0,0 +1,4 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("GenomicRanges", "hiAnnotator", "Biostrings", "ShortRead", 
+	 "Rsamtools", "BSgenome", "BSgenome.Hsapiens.UCSC.hg38"))
+q()
