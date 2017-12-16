f7d1dbfc4ab893e4d40d240a3df383cf7d1c3091
Christopher Nobles <cnobles@upenn.edu>
2017-12-16 15:44:48 -0500

    Updated .travis.yml with test script.

---
+++ tests/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Test script
+source activate idsb
+
+# Create test analysis directory
+snakemake analysis/test --configfile configs/test.config.yml -np
+snakemake analysis/test --configfile configs/test.config.yml
+
+# Move test sequence files to analysis directory
+cp tests/Data/Undetermined_S0_L001_* analysis/test/input_data/
+
+# Generate test DAG graph
+snakemake --configfile configs/test.config.yml -np
+snakemake --configfile configs/test.config.yml --dag | dot -Tsvg > test.dag.svg
+snakemake --configfile configs/test.config.yml --latency-wait 30
+cat analysis/test/process/processedData/unique_sites.test.csv
+
