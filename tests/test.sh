cbc8c789b6f278214a35ee2ab5a46df1398406b3
Christopher Nobles <cnobles@upenn.edu>
2017-12-17 09:44:52 -0500

    Implemented Travis specific test.

--- tests/test.sh
+++ tests/test.sh
@@ -7,14 +7,14 @@ export PATH=${PATH}:${PREFIX}/bin
 source activate idsb
 
 # Create test analysis directory
-snakemake analysis/test --configfile configs/test.config.yml -np
-snakemake analysis/test --configfile configs/test.config.yml
+snakemake analysis/travis --configfile configs/travis.config.yml -np
+snakemake analysis/travis --configfile configs/travis.config.yml
 
 # Move test sequence files to analysis directory
-cp tests/Data/Undetermined_S0_L001_* analysis/test/input_data/
+cp tests/Data/Undetermined_S0_L001_* analysis/travis/input_data/
 
 # Generate test DAG graph
-snakemake --configfile configs/test.config.yml -np
-snakemake --configfile configs/test.config.yml --dag | dot -Tsvg > test.dag.svg
-snakemake --configfile configs/test.config.yml --latency-wait 30
-cat analysis/test/process/processedData/unique_sites.test.csv
+snakemake --configfile configs/travis.config.yml -np
+snakemake --configfile configs/travis.config.yml --dag | dot -Tsvg > travis.dag.svg
+snakemake --configfile configs/travis.config.yml --latency-wait 30
+cat analysis/travis/process/processedData/unique_sites.travis.csv
