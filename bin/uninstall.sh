8d08d2ac6c9a93193d8b5c80009ff2624a927dc2
Chris Nobles <chris.l.nobles@gmail.com>
2018-02-22 14:01:09 -0500

    Name change.

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 # Remove created environment for iDSBseq
 source deactivate
-conda env remove -n idsb --yes
+conda env remove -n iguide --yes
 conda config --remove channels 'bushmanlab'
 conda config --remove channels 'bioconda'
 conda config --remove channels 'r'
