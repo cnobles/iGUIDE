03db0872a059a6fb5c60734a3f212b2da04aced1
Christopher Nobles <cnobles@pennmedicine.upenn.edu>
2018-05-11 14:06:11 -0400

    Modified the additional high.par snakefile.

--- tools/rtools/parse_input_seqs.R
+++ tools/rtools/parse_input_seqs.R
@@ -1,9 +1,10 @@
-seq_file <- ShortRead::readFastq(snakemake@input[[1]])
+args <- commandArgs(trailingOnly = TRUE)
+seq_file <- ShortRead::readFastq(args[1])
 seq_file <- split(
   seq_file, ceiling(
-    seq_along(seq_file)/(length(seq_file)/snakemake@params[[1]])))
-input_path <- unlist(stringr::str_split(snakemake@input[[1]], "/"))
-output_path <- paste(input_path[1:(length(input_path) - 1)], collapse = "/")
+    seq_along(seq_file)/(length(seq_file)/as.numeric(args[3]))))
+input_path <- unlist(stringr::str_split(args[1], "/"))
+output_path <- args[2]
 sample_name <- unlist(
   stringr::str_split(tail(input_path, n = 1), stringr::fixed(".")))
 sample_name <- paste(sample_name[1:2], collapse = ".")
@@ -11,7 +12,7 @@ null <- lapply(seq_along(seq_file), function(i){
   sam_name <- paste0(
     sample_name, ".chunk", stringr::str_pad(i, 2, pad = "0"), ".fastq.gz")
   ShortRead::writeFastq(
-    object = seq_file[[i]], 
-    file = file.path(output_path, sam_name), 
+    object = seq_file[[i]],
+    file = file.path(output_path, sam_name),
     compress = TRUE)
 })
