e52161307dda5a377aa1661e712191a0cb1959d6
Christopher Nobles <cnobles@pennmedicine.upenn.edu>
2018-06-05 09:56:44 -0400

    Settled on environment creation method and requirement file.

--- bin/setup.R
+++ bin/setup.R
@@ -1,6 +1,6 @@
 # Install required R-packages, downloaded from CRAN Mirror snapshot
 r_libs <- c(
-  "argparse", "data.table", "devtools", "igraph", "knitr", "magrittr", "Matrix", 
+  "argparse", "data.table", "devtools", "igraph", "knitr", "magrittr", "Matrix",
   "pander", "RColorBrewer", "reshape2", "rmarkdown", "scales", "tidyverse", 
   "yaml", "kableExtra")
 get_r_libs <- r_libs[!r_libs %in% row.names(installed.packages())]
@@ -17,7 +17,10 @@ bioc_libs <- c(
   "GenomicRanges", "hiAnnotator", "IRanges", "Rsamtools", "ShortRead")
 get_bioc_libs <- bioc_libs[!bioc_libs %in% row.names(installed.packages())]
 if(length(get_bioc_libs) > 0){
-  biocLite(get_bioc_libs, suppressUpdates = TRUE, ask = FALSE) }
+  biocLite(
+    get_bioc_libs, 
+    suppressUpdates = TRUE, ask = FALSE,
+    siteRepos = "https://mran.microsoft.com/snapshot/2018-05-01/") }
 
 # Install developmental based packages from github
 options(unzip = "internal")
@@ -25,7 +28,7 @@ devtools::install_github(
   "cnobles/gintools", 
   repos = "https://mran.microsoft.com/snapshot/2018-05-01/",
   upgrade_dependencies = FALSE,
-  dependencies = c("Depends", "Imports"))
+  dependencies = FALSE)
 
 # Check for installed packages
 all_required <- c(r_libs, bioc_libs, "gintools")
