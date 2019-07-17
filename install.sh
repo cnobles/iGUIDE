657bcf0c95de5d3b4f947070e72de96239b6bdae
Christopher Nobles <cnobles@pennmedicine.upenn.edu>
2019-07-17 13:34:21 -0400

    Install bugfix 2.

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ function __detect_conda_install() {
 
     if [[ $discovered = true ]]; then
       	local conda_path="$(which conda)"
-        local conda_path=${conda_path%'/condabin/conda'}
+        conda_path=${conda_path%'/condabin/conda'}
         echo ${conda_path%'/bin/conda'}
     fi
 }
